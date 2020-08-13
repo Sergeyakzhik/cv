c360dd109b44844e59aa8c740a678e7ed851e20b
Sergei <sergeyakzhik@gmail.com>
2020-08-13 21:27:57 +0300

    Updates

---
+++ precache-manifest.ef50d898719f4c13c4cee040daa946da.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76f7ca53445a5a5b584408153181bad6",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "3cf0499e61a1045dd5d8",
+    "url": "/cv/static/css/main.babfc6b0.chunk.css"
+  },
+  {
+    "revision": "c5ed88fc5a18d02c85e0",
+    "url": "/cv/static/js/2.b6bf884e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/cv/static/js/2.b6bf884e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cf0499e61a1045dd5d8",
+    "url": "/cv/static/js/main.96a02768.chunk.js"
+  },
+  {
+    "revision": "9e0e921495960402e4a4",
+    "url": "/cv/static/js/runtime-main.c17789b0.js"
+  },
+  {
+    "revision": "4b562f63e9a891ba82363ad701445e69",
+    "url": "/cv/static/media/avatar.4b562f63.jpg"
+  },
+  {
+    "revision": "454a3633bd7872c8b1ba588e74f2fdc1",
+    "url": "/cv/static/media/facebook.454a3633.svg"
+  },
+  {
+    "revision": "0ff65a01266d30bcd070b19b6ebcda07",
+    "url": "/cv/static/media/linkedin.0ff65a01.svg"
+  }
+]);
